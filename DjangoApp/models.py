fc4c56b3a21e2acba657681450cccbe185943450
martinkagema01@gmail.com <martinkagema01@gmail.com>
2024-11-21 10:26:53 +0300

    Initial commit

--- DjangoApp/models.py
+++ DjangoApp/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class  Members(models.Model):
+class  Member(models.Model):
     fname = models.CharField('First Name', max_length=50)
     lname = models.CharField('Last Name', max_length=50)
     email = models.EmailField(max_length=50)
     passwd = models.CharField(max_length=50)
     age = models.IntegerField()
     
+    def __str__(self):
+        return self.fname
